60814d05a32d0224b0a05a78328fa2efaf5f65b8
boco <elizaboco66@gmail.com>
2020-09-09 10:17:06 +0200

    modif

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark shadow-sm" style="background-color:teal;" ><!--navbar-expand-md navbar-dark bg-primary shadow-sm -->
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-sm" >
             <div class="container-fluid">
                 <a class="navbar-brand" style="color:red;" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
